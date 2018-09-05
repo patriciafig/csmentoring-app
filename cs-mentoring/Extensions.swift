5fed8cb278b1301306eecc17a73c073f0c3844db
patriciafig <patricia.figueroa@ucdenver.edu>
2018-09-05 15:36:12 -0600

    adding extensions to UIColor, for app style

---
+++ cs-mentoring/Extensions.swift
@@ -0,0 +1,43 @@
+//
+//  Extensions.swift
+//  cs-mentoring
+//
+//  Created by Patricia on 9/4/18.
+//  Copyright © 2018 Patricia Figueroa. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    // Represents the SeaBlue #5AE1FC, with RGB values of (90, 255, 252
+    static var SeaBlue: UIColor {
+        get {
+            return UIColor(red: 90.0/255.0, green: 255.0/255.0, blue: 252.0/255.0, alpha: alpha)
+        }
+    }
+    
+    // Represents the color SkyBlue #2699FB, whose RGB values are (38, 153, 251)
+    static var SkyBlue: UIColor {
+        get {
+            return UIColor(red: 38.0/255.0, green: 153.0/255.0, blue: 251.0/255.0, alpha: alpha)
+        }
+    }
+    
+    // Represents the color RoyalPurple #6759FF, whose RGB values are (103, 89, 255)
+    static var RoyalPurple: UIColor {
+        get {
+            return UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+        }
+    }
+    //Represents the color PlumPurple #35293F, whose RGB values are (53, 41, 63)
+    static var PlumPurple: UIColor {
+        get {
+            return UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+        }
+        
+    }
+
+}
+
+
+
