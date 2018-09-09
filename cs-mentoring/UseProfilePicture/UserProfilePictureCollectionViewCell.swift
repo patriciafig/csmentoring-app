c9132488f398555ce518dcba776a988e29bb7462
patriciafig <patricia.figueroa@ucdenver.edu>
2018-09-09 15:49:07 -0600

    user profile done

---
+++ cs-mentoring/UseProfilePicture/UserProfilePictureCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserProfilePictureCollectionViewCell.swift
+//  cs-mentoring
+//
+//  Created by Patricia on 9/7/18.
+//  Copyright © 2018 Patricia Figueroa. All rights reserved.
+//
+
+import UIKit
+
+class UserProfilePictureCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var profilePic: UIImageView!
+}
+ 
